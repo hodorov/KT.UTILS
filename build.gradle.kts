b050457fcda635a20d2e4bb277dc960b4959ffc9
el1191 <el1191@woyd.ru>
2019-05-11 03:50:07 +0300

    Add maven plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     val kotlinVersion = "1.3.31"
-
+    
+    maven
     id("io.spring.dependency-management") version "1.0.6.RELEASE"
     id("org.jetbrains.kotlin.jvm") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
